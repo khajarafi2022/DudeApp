767ca4fe0fddaf6c5370ba3a89c58c35ce8105c1
khajarafi <=>
2026-01-06 19:08:32 +0530

    first commit

---
+++ datatypes.js
@@ -0,0 +1,9 @@
+//datatypes in javascript
+var empid = 1001; //number
+console.log(typeof(empid));
+var emobilenumber = 9554654755;
+console.log(typeof(emobilenumber));
+var esalary = 25000.465;
+console.log(typeof(esalary));
+var ename = "rahul";
+console.log(typeof(ename));
---
+++ demo.js
@@ -0,0 +1,12 @@
+let firstnumber=10;
+let secodnumber=20;               
+console.log("sum is:"+(a+b));
+
+//addtion of two numbers
+let firstNumber = 10;
+let secondNumber = 20;         
+console.log("sum is:"+(a+b));
+ 
+
+
+
